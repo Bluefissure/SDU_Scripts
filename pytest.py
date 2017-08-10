5ebd9eaed135189226982b92ed8092e4427663ff
BlueFissure <306401806@qq.com>
2017-08-10 11:17:17 +0800

    add test

---
+++ pytest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3 
+#coding=utf-8
+#Author: Bluefissure
+import sys
+import urllib
+import urllib.request
+import urllib.parse
+import getpass
+import json
+import http.cookiejar
+import time
+urlbase='http://bkjwxk.sdu.edu.cn'
+#urlbase='http://202.194.15.19' #If domain cannot be accessed
