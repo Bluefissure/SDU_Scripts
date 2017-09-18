3b638cb767855430076db08b4d26f2dec58528c6
BlueFissure <306401806@qq.com>
2017-09-18 12:45:20 +0800

    update md5

--- xuanke.py
+++ xuanke.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3 
 #coding=utf-8
-# XuankeScript
-# Copyright (C) 2017  Bluefissure
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#Author: Bluefissure
 import sys
 import urllib
 import urllib.request
@@ -27,7 +13,9 @@ urlbase='http://bkjwxk.sdu.edu.cn'
 #urlbase='http://202.194.15.19' #If domain cannot be accessed
 def login(username,password):
 	urlLogin=urlbase+'/b/ajaxLogin'
-	
+	m = md5()
+	m.update(password.encode())
+	password = m.hexdigest()
 	data = {
 		"j_username": username,
 		"j_password": password,
